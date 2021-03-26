c49ebce25b8233320516fc02c249336e1eeff453
Suat Ozcan <41187131+SuatOzcan@users.noreply.github.com>
2021-03-26 13:49:38 +0300

    Update Create Dungeon. Add left and right leaves.
    Divides the cubes in half and paints them in different colors.

--- Assets/CreateDungeon.cs
+++ Assets/CreateDungeon.cs
@@ -8,11 +8,18 @@ public class CreateDungeon : MonoBehaviour
     public int mapDepth = 50;
     public int scale = 2;
     Leaf root;
+    Leaf left;
+    Leaf right;
     // Start is called before the first frame update
     void Start()
     {
         root = new Leaf(0, 0, mapWidth, mapDepth, scale);
-        root.Draw();
+       // root.Draw();
+        left = new Leaf(0, 0, mapWidth / 2, mapDepth / 2, scale);
+        right = new Leaf(mapWidth/2, 0, mapWidth / 2, mapDepth / 2, scale);
+        left.Draw();
+        right.Draw();
+
     }
 
     // Update is called once per frame
