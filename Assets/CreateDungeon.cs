5553364e6745d4fafeafefe339188c2da9100cdc
Suat Ozcan <41187131+SuatOzcan@users.noreply.github.com>
2021-03-26 13:32:25 +0300

    Add Draw Cubes Method in Leaf na instanciate them with CreateDungeon.

--- Assets/CreateDungeon.cs
+++ Assets/CreateDungeon.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class CreateDungeon : MonoBehaviour
 {
-    public int mapWidth;
-    public int mapDepth;
+    public int mapWidth = 50;
+    public int mapDepth = 50;
+    public int scale = 2;
+    Leaf root;
     // Start is called before the first frame update
     void Start()
     {
-        
+        root = new Leaf(0, 0, mapWidth, mapDepth, scale);
+        root.Draw();
     }
 
     // Update is called once per frame
--- Assets/Leaf.cs
+++ Assets/Leaf.cs
@@ -2,17 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Leaf : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+public class Leaf
+{   int xpos;
+    int zpos;
+    int width;
+    int depth;
+    int scale;
+
+ public Leaf(int x, int z, int w,int d,int s)
     {
-        
+        xpos = x;
+        zpos = z;
+        width = w;
+        depth = d;
+        scale = s;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Draw()
     {
-        
+        Color c = new Color(Random.Range(0, 1f), Random.Range(0, 1f), Random.Range(0, 1f));
+        for (int x = xpos; x < width + xpos; x++)      
+            for (int z = zpos; z < depth + zpos; z++)
+            {
+                GameObject Cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                Cube.transform.position = new Vector3(x * scale, 0, z * scale);
+                Cube.transform.localScale = new Vector3(scale, scale, scale);
+                Cube.GetComponent<Renderer>().material.SetColor("_Color", c);
+            }
     }
 }
